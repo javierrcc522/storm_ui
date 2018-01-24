704459932b190bbf3d154efbc1ae1c8013695b7f
Javier Jimenez <javierrcc522@gmail.com>
2018-01-24 12:03:12 -0800

    add a request and jsonify from flask, and a post method

--- strom_ui.py
+++ strom_ui.py
@@ -1,9 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
-   return render_template("index.html")
+    return render_template('index.html')
+
+@app.route('/nameTemplate', methods=['POST'])
+def name_template():
+    name = request.form['name']
+    if name:
+        newName = str(name).title()
+        return jsonify({'name' : newName})
+    return jsonify({'error' : 'Missing Data!'})
 
 if __name__ == '__main__':
    app.run(debug = True)
